d1bba6c75f3c72d813bed7421b39582eefbb96c9
PupilCurious <ertugrul.bilal78@gmail.com>
2023-02-27 05:39:49 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        int number, first=0, second=1;
+        Scanner input = new Scanner(System.in);
+        System.out.print("Eleman Sayısını Giriniz: ");
+        number=input.nextInt();
+
+        for(int i=0; i<=number; i++){
+            System.out.print(first + " ");
+            int total = first + second;
+            first = second;
+            second = total;
+
+        }
+    }
+}
